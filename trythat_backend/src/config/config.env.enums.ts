782156b849c79a075fac2ef24cbc99d902481b5e
Ashwini Anil Pathak <ashwinipathak991@gmail.com>
2025-07-02 18:08:28 +0530

    Create config.env.enums.ts

---
+++ trythat_backend/src/config/config.env.enums.ts
@@ -0,0 +1,7 @@
+export enum ConfigEnv {
+  FRONTEND_URL = 'https://app.trythat.aribadevops.site',
+  HOST_URL = 'https://bknd.trythat.aribadevops.site/v1',
+  R_ENGINE_HOST = 'http://lgsearch2.qodequay.com/vapi/search_service',
+  GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api',
+  GOOGLE_API_KEY = 'AIzaSyBcUKoJDAVwlAI97RPABaC603CGh9Jr1cc',
+}
