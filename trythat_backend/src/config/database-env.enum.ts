d7644202f85bc319df1ccac188a00cfff0b7766a
Ashwini Anil Pathak <ashwinipathak991@gmail.com>
2025-07-02 15:03:05 +0530

    Update database-env.enum.ts

--- trythat_backend/src/config/database-env.enum.ts
+++ trythat_backend/src/config/database-env.enum.ts
@@ -2,3 +2,10 @@ export enum DatabaseEnv {
   DB_USER_NAME = 'tt_uapp',
   DB_USER_CONN = 'UAPP_DB',
 }
+
+export enum ConfigEnv {
+  DEVELOPMENT = 'development',
+  PRODUCTION = 'production',
+  STAGING = 'staging',
+  TEST = 'test',
+}
